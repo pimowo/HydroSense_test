c783666b9d9579fc5243e18a0102d9e806397842
pimowo <124725348+pimowo@users.noreply.github.com>
2024-11-15 02:55:27 +0100

    Update HydroSense_mini.ino

--- HydroSense_mini.ino
+++ HydroSense_mini.ino
@@ -108,12 +108,10 @@ bool isPumpActive = false; // Status pracy pompy
     bool soundEnabled = false; // Status włączenia dźwięku alarmu
     bool isServiceMode = false; // Status trybu serwisowego
     unsigned long lastSuccessfulMeasurement = 0; // Czas ostatniego udanego pomiaru
-    //unsigned long lastSuccessfulMQTTPublish = 0; // Czas ostatniej udanej publikacji MQTT
 } status;
 
 // Struktura dla obsługi przycisku
 struct ButtonState {
-    //bool currentState = HIGH; // Aktualny stan przycisku
     bool lastState; // Poprzedni stan przycisku
     unsigned long pressedTime = 0; // Czas wciśnięcia przycisku
     unsigned long releasedTime = 0; // Czas puszczenia przycisku
